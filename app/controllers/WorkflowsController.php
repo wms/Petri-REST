e38c0595fdede48517dc740b8e1d401f3bfc15cb
Warren Seymour <wms@fountainhead-solutions.net>
2011-09-24 00:00:23 +0100

    Add Workflows controller using RESTful actions

---
+++ app/controllers/WorkflowsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Workflows;
+use lithium\action\DispatchException;
+
+class WorkflowsController extends \lithium\action\Controller {
+
+	public function index() {
+		$workflows = Workflows::all();
+		return compact('workflows');
+	}
+
+	public function view() {
+		$workflow = Workflows::first($this->request->id);
+		return compact('workflow');
+	}
+
+	public function add() {
+		$workflow = Workflows::create();
+		$result = $workflow->save($this->request->data);
+
+		return compact('result', 'workflow');
+	}
+
+	public function edit() {
+		$workflow = Workflows::find($this->request->id);
+
+		if (!$workflow) {
+            //exception
+            return false;
+		}
+        $result = $workflow->save($this->request->data);
+
+		return compact('result', 'workflow');
+	}
+
+	public function delete() {
+		$result = Workflows::find($this->request->id)->delete();
+
+        return compact('result');
+	}
+}
+
+?>
