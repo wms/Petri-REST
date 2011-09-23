c239923247128e099a75c8668a26699e2cac4fa5
Warren Seymour <wms@fountainhead-solutions.net>
2011-09-24 00:01:21 +0100

    Add Workflows models and tests

---
+++ app/models/Workflows.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+class Workflows extends \lithium\data\Model {
+
+	public $validates = array();
+}
+
+?>
---
+++ app/tests/cases/models/WorkflowsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\tests\cases\models;
+
+use app\models\Workflows;
+
+class WorkflowsTest extends \lithium\test\Unit {
+
+	public function setUp() {}
+
+	public function tearDown() {}
+
+
+}
+
+?>
