6757907a83b75195989ce20ae0c3108c73a726b6
warrenseymour <wms@fountainhead-solutions.net>
2012-01-01 23:17:25 +0000

    Add standard properties to Transition model

--- app/webroot/gui/js/models/transition.js
+++ app/webroot/gui/js/models/transition.js
@@ -3,7 +3,11 @@ define([
     'Backbone',
 ], function(_, Backbone) {
     var transitionModel = Backbone.Model.extend({
+        idAttribute: '_id',
 
+        parse: function(response) {
+            return response.transition;
+        }
     });
 
     return transitionModel;
