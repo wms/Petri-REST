d6648722ebaae31427ee8ce10855e28f4e53e66e
Warren Seymour <wms@fountainhead-solutions.net>
2012-05-09 12:13:11 +0100

    Revert Place properties on bad save

--- app/webroot/gui/js/models/place.js
+++ app/webroot/gui/js/models/place.js
@@ -6,8 +6,42 @@ define([
         urlRoot: '/pr/places',
         idAttribute: '_id',
 
+        initialize: function() {
+            _.bindAll(this, 'prepare_revert', 'revert');
+            this.prepare_revert();
+        },
+
         parse: function(response) {
             return response.place ? response.place : response;
+        },
+
+        save: function(attributes, options) {
+            var self = this;
+            var options = options || {};
+            var error = options.error;
+            this.prepare_revert();
+
+            options.error = function(model, response) {
+                self.trigger("save:error", self);
+                if(error) {
+                    error(self, response)
+                }
+                self.revert();
+            }
+            this.trigger('save', this);
+
+            result = Backbone.Model.prototype.save.call(this, attributes, options);
+            return result;
+        },
+
+        prepare_revert: function() {
+            this._revertAttributes = _.clone(this.attributes);
+        },
+
+        revert: function() {
+            if(this._revertAttributes) {
+                this.set(this._revertAttributes);
+            }
         }
     });
 
